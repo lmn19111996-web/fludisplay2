29c73a82e840a5584d4a8e930b9248844e02f4f5
lmn19111996-web <lmn19111996@gmail.com>
2025-12-04 21:29:00 +0100

    Update git pull command with repository URL

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ if [ ! -d ".git" ]; then
     git clone https://github.com/lmn19111996-web/fludisplay2.git
 else
     # Wenn bereits geklont, dann einfach pullen
-    git pull
+    git pull https://github.com/lmn19111996-web/fludisplay2.git
 fi
 
 # Node-Server starten
