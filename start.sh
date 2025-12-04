128062722e7c9de1da0322b8fe07a0e3a5d13dae
lmn19111996-web <lmn19111996@gmail.com>
2025-12-04 21:28:29 +0100

    final2
    Change git clone command to clone into a directory instead of the current one.

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ cd /opt/stacks
 
 # Wenn das Repo noch nicht geklont wurde → klonen
 if [ ! -d ".git" ]; then
-    git clone https://github.com/lmn19111996-web/fludisplay2.git .
+    git clone https://github.com/lmn19111996-web/fludisplay2.git
 else
     # Wenn bereits geklont, dann einfach pullen
     git pull
