5c363492773724800112e0406d8013661b4238ff
lmn19111996-web <lmn19111996@gmail.com>
2025-12-04 23:13:44 +0100

    Refactor start.sh for repo management and setup
    Updated script to clone repo with sudo if not present, and added steps for pulling updates and installing dependencies.

--- start.sh
+++ start.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# Arbeitsverzeichnis wechseln
 cd /opt/stacks
 
-# Wenn das Repo noch nicht geklont wurde → klonen
-if [ ! -d ".git" ]; then
-    git clone https://github.com/lmn19111996-web/fludisplay2.git
-else
-    # Wenn bereits geklont, dann einfach pullen
-    cd /opt/stacks/fludisplay2
-    git pull https://github.com/lmn19111996-web/fludisplay2.git
+# Wenn das Repo nicht existiert → mit sudo klonen
+if [ ! -d "fludisplay2" ]; then
+    sudo git clone https://github.com/lmn19111996-web/fludisplay2.git
 fi
 
-# Node-Server starten
+# In das Repo wechseln
+cd fludisplay2
+
+# Updates holen (sudo nötig)
+sudo git pull https://github.com/lmn19111996-web/fludisplay2.git
+
+# Dependencies installieren (sudo für node_modules)
+sudo npm install --production
+
+# Node-Server starten (kein sudo nötig)
 exec node server.js
